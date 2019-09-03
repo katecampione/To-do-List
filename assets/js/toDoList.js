a7bea9421e5cf8f31e748a8256c27a70be8015b4
Kate Campione <katecampione@gmail.com>
2019-09-03 17:48:05 -0600

    functionality is setup

---
+++ assets/js/toDoList.js
@@ -0,0 +1,21 @@
+//check off specific todos by lcicking
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+//click on x to delete todo
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+   if(event.which === 13){
+     //grab new todo text from input
+     var todoText = $(this).val();
+     $(this).val("");
+     //create new li and add to ul
+     $('ul').append("<li><span>X</span> " + todoText + "</li>");
+   }
+});
